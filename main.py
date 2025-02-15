f6163f5a4891fbd24cacfb4b9d640a082dbd6d75
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-02-15 14:33:36 -0600

    created the player and drew it on the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,9 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
+    player_x = SCREEN_WIDTH/2
+    player_y = SCREEN_HEIGHT/2
+    player = Player(player_x, player_y)
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +19,7 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
