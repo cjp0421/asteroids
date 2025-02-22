5a4f8ec9bf6f7291394908b9e9dc8894446d3e3b
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-02-22 15:41:01 -0600

    add asteroid splitting functionality

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
             for shot in shots:
                 if asteroid.collision_check(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             if player.collision_check(asteroid):
                 sys.exit("Game over!")
 
